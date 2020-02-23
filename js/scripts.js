7efead0bfe0589683803ddf301c91dedcc321cc5
Brenda-M <brendanjuguna1@gmail.com>
2020-02-23 17:07:28 +0300

    add a submit section

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,5 @@
-$(document).ready(function(){
-    $(".btn1").click(function(){
-      $("p").hide();
-    });
-    $(".btn2").click(function(){
-      $("p").show();
-    });
-  });
 
-  function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  } 
+
+
+
+
